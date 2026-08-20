2952f9832d0ab83fb051463d2f0674efd356887b
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-20 11:06:33 +0200

    Format and require Swift 6.4

--- Package.swift
+++ Package.swift
@@ -1,15 +1,15 @@
-// swift-tools-version: 6.3.3
+// swift-tools-version: 6.4
 
 import PackageDescription
 
 let package = Package(
     name: "swift-complex-primitives",
     platforms: [
-        .macOS("27"),
-        .iOS("27"),
-        .tvOS("27"),
-        .watchOS("27"),
-        .visionOS("27"),
+        .macOS(.v27),
+        .iOS(.v27),
+        .tvOS(.v27),
+        .watchOS(.v27),
+        .visionOS(.v27),
     ],
     products: [
         .library(name: "Complex Primitives", targets: ["Complex Primitives"]),
@@ -19,9 +19,18 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/swift-primitives/swift-numeric-primitives.git", branch: "main"),
-        .package(url: "https://github.com/swift-primitives/swift-dimension-primitives.git", branch: "main"),
-        .package(url: "https://github.com/swift-primitives/swift-tagged-primitives.git", branch: "main"),
+        .package(
+            url: "https://github.com/swift-primitives/swift-numeric-primitives.git",
+            branch: "main"
+        ),
+        .package(
+            url: "https://github.com/swift-primitives/swift-dimension-primitives.git",
+            branch: "main"
+        ),
+        .package(
+            url: "https://github.com/swift-primitives/swift-tagged-primitives.git",
+            branch: "main"
+        ),
     ],
     targets: [
         .target(
@@ -36,7 +45,10 @@ let package = Package(
             name: "Complex Primitives Test Support",
             dependencies: [
                 "Complex Primitives",
-                .product(name: "Tagged Primitives Test Support", package: "swift-tagged-primitives"),
+                .product(
+                    name: "Tagged Primitives Test Support",
+                    package: "swift-tagged-primitives"
+                ),
             ],
             path: "Tests/Support"
         ),
@@ -44,7 +56,10 @@ let package = Package(
             name: "Complex Primitives Tests",
             dependencies: [
                 "Complex Primitives",
-                .product(name: "Tagged Primitives Standard Library Integration", package: "swift-tagged-primitives"),
+                .product(
+                    name: "Tagged Primitives Standard Library Integration",
+                    package: "swift-tagged-primitives"
+                ),
                 "Complex Primitives Test Support",
             ]
         ),
